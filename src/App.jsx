cb8b0997fbb19bfef7636c7f2b53371b6fc04e7b
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-05 15:52:47 +0500

    events and loops onclick, onDoubleclick, eventhandling etc done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import "./App.css"
-import "./index.css"
-const App = () => {
-  // const headingStyle = {
-  //   color : "green",
-  //   fontSize : "23px"
+// import React from 'react'
+// import "./App.css"
+// import "./index.css"
+// const App = () => {
+//   // const headingStyle = {
+//   //   color : "green",
+//   //   fontSize : "23px"
 
-  // }
-  return (
-    <div>
-      <h1 style={{height:"40px",color:"green"}}>hello everyone</h1>
+//   // }
+//   return (
+//     <div>
+//       <h1 style={{height:"40px",color:"green"}}>hello everyone</h1>
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
+
+// import React from 'react'
+
+// const App = () => {
+//   const onText = () => console.log("mouse on text")
+//   const double = () => console.log("mouse double clicked")
+//   const handlerInput = (event) =>{
+//      console.clear();
+//     console.log("value:", event.target.value);
+//   }
+//   return (
+    
+//     <div>
+//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, perferendis? At reiciendis iusto repellat voluptatem provident? Molestiae autem ut accusamus quis atque laboriosam ab, nisi vel, nam sunt provident asperiores inventore fuga voluptatem totam.</p>
+//       <button onMouseOver={onText} onDoubleClick={double}>sample button</button>
+//       <input type="text" placeholder='typesomething' onChange={handlerInput}/>
+//     </div>
+//   )
+// }
+
+// export default App
