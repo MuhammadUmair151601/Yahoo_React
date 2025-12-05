aed58debb472d9e41ebfdc03d1b01c46dc287685
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-05 10:24:01 +0500

    inline internal and external css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import "./App.css"
+import "./index.css"
 const App = () => {
+  // const headingStyle = {
+  //   color : "green",
+  //   fontSize : "23px"
+
+  // }
   return (
     <div>
+      <h1 style={{height:"40px",color:"green"}}>hello everyone</h1>
       
     </div>
   )
