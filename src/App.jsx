81426e8ae3180cfd5b391e322b58197a234f1f86
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-05 16:49:27 +0500

    array and object map method done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// ********************************CSS styling in react JS
+
 // import React from 'react'
 // import "./App.css"
 // import "./index.css"
@@ -17,6 +19,8 @@
 
 // export default App
 
+
+// ***************************Events and functions
 // import React from 'react'
 
 // const App = () => {
@@ -32,6 +36,44 @@
 //       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, perferendis? At reiciendis iusto repellat voluptatem provident? Molestiae autem ut accusamus quis atque laboriosam ab, nisi vel, nam sunt provident asperiores inventore fuga voluptatem totam.</p>
 //       <button onMouseOver={onText} onDoubleClick={double}>sample button</button>
 //       <input type="text" placeholder='typesomething' onChange={handlerInput}/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// **************************Map method using array and objects
+
+// import React from 'react'
+
+// const App = () => {
+//   const students = ["ali","ahmed","kamran","taimoor"]
+//   return (
+//     <div>
+//       {students.map((student,index)=>{
+//         return <li key={index}>{index}::{student}</li>
+//       })}
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+
+// const App = () => {
+//   const arr = [
+//     {name:"ali", age:23, city: "peshawar" },
+//     {name:"adnan", age:23, city: "peshawar" },
+//     {name:"luqman", age:23, city: "peshawar" },
+//     {name:"shayan", age:23, city: "peshawar" }
+//   ]
+//   return (
+//     <div>
+//         {arr.map((array,index)=>{
+//          return <h1 key={index}>{index} The detail of {array.name} is age:{array.age} and city:{array.city}</h1>
+//         })}
 //     </div>
 //   )
 // }
