2f73e7b8af7679668684802083228a2828496d62
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-09 18:36:30 +0500

    usestate using forms done:

--- src/components/Multiplestates.jsx
+++ src/components/Multiplestates.jsx
@@ -1,20 +1,35 @@
+// import React, { useState } from 'react'
+
+// const Multiplestates = () => {
+//   const [name, setname] = useState("Ali")
+//   const [age, setage] = useState(18)
+//   const changeName = ()=>{
+//     setname("Ali Khan")
+//   }
+//   const changeAge = ()=>{
+//     setage(age+1)
+//   }
+//   return (
+//     <div>
+//       <h1>Name:{name}</h1>
+//       <h2>Age:{age}</h2>
+//       <button onClick={changeName}>Change Name</button>
+//       <button onClick={changeAge}>Change Age</button>
+//     </div>
+//   )
+// }
+
+// export default Multiplestates
+
+// ***************Forms in usestate 
 import React, { useState } from 'react'
 
 const Multiplestates = () => {
-  const [name, setname] = useState("Ali")
-  const [age, setage] = useState(18)
-  const changeName = ()=>{
-    setname("Ali Khan")
-  }
-  const changeAge = ()=>{
-    setage(age+1)
-  }
+  const [name, setname] = useState("")
   return (
     <div>
-      <h1>Name:{name}</h1>
-      <h2>Age:{age}</h2>
-      <button onClick={changeName}>Change Name</button>
-      <button onClick={changeAge}>Change Age</button>
+      <input type="text" value={name} onChange={(e)=> setname(e.target.value)}/>
+      <p>Hello, {name}</p>
     </div>
   )
 }
